16aff67d95e0912c0a7d9dd594487459b9a6d8c5
BBtat23 <beasubi@gmail.com>
2018-07-22 11:32:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bea/bakingapp/widget/ListItem.java
@@ -0,0 +1,8 @@
+package com.example.bea.bakingapp.widget;
+
+public class ListItem {
+	public String heading,content;
+
+	public String[] contentString = new String[]{"hola","prueba","adios"};
+
+}
---
+++ app/src/main/java/com/example/bea/bakingapp/widget/ListProvider.java
@@ -0,0 +1,109 @@
+package com.example.bea.bakingapp.widget;
+
+import java.util.ArrayList;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService.RemoteViewsFactory;
+import com.example.bea.bakingapp.R;
+import com.example.bea.bakingapp.data.Recipe;
+
+/**
+ * If you are familiar with Adapter of ListView,this is the same as adapter
+ * with few changes
+ * 
+ */
+public class ListProvider implements RemoteViewsFactory {
+	private ArrayList<ListItem> listItemList = new ArrayList<ListItem>();
+	private Context context = null;
+	private int appWidgetId;
+	private ArrayList<Recipe> mRecipes;
+		    String contentItemString = "";
+		    StringBuilder builder = new StringBuilder();
+
+	public ListProvider(Context context, Intent intent) {
+		this.context = context;
+		appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+				AppWidgetManager.INVALID_APPWIDGET_ID);
+
+		populateListItem();
+	}
+
+	private void populateListItem() {
+		Recipe recipe = new Recipe();
+		Log.d("ListProvider", "recipes" + recipe.getName());
+		String recipeName = recipe.getName();
+		ListItem listItem = new ListItem();
+		String[] contentString = listItem.contentString;
+		for (String contentItem : contentString) {
+			builder.append("- " + contentItem + "\n");
+		}
+		listItem.heading = recipeName;
+//    for (int i = 0; i < 10; i++) {
+//    ListItem listItem = new ListItem();
+//    listItem.heading = "Heading" + i;
+//    listItem.content = i
+//    + " This is the content of the app widget listview.Nice content though";
+		listItemList.add(listItem);
+	}
+
+
+	@Override
+	public int getCount() {
+		return listItemList.size();
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	/*
+	 *Similar to getView of Adapter where instead of View
+	 *we return RemoteViews 
+	 * 
+	 */
+	@Override
+	public RemoteViews getViewAt(int position) {
+		final RemoteViews remoteView = new RemoteViews(
+				context.getPackageName(), R.layout.list_row);
+		ListItem listItem = listItemList.get(position);
+//		remoteView.setTextViewText(R.id.heading, listItem.heading);
+		remoteView.setTextViewText(R.id.content, builder.toString());
+		remoteView.setTextViewText(R.id.heading, listItem.heading);
+
+		return remoteView;
+	}
+	
+
+	@Override
+	public RemoteViews getLoadingView() {
+		return null;
+	}
+
+	@Override
+	public int getViewTypeCount() {
+		return 1;
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		return true;
+	}
+
+	@Override
+	public void onCreate() {
+	}
+
+	@Override
+	public void onDataSetChanged() {
+	}
+
+	@Override
+	public void onDestroy() {
+	}
+
+}
